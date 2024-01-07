ca59dcd571998da7596f4ef07e67578431ae7272
Jason Chafin <jchafin@ucsc.edu>
2024-01-07 09:39:44 -0800

    lint-fix

--- functions.php
+++ functions.php
@@ -94,24 +94,28 @@ function casaluna_register_block_pattern_categories() {
 add_action( 'init', 'casaluna_register_block_pattern_categories' );
 
 // Wp v4.7.1 and higher
-add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
-  $filetype = wp_check_filetype( $filename, $mimes );
-  return [
-      'ext'             => $filetype['ext'],
-      'type'            => $filetype['type'],
-      'proper_filename' => $data['proper_filename']
-  ];
+add_filter(
+	'wp_check_filetype_and_ext',
+	function ( $data, $file, $filename, $mimes ) {
+		$filetype = wp_check_filetype( $filename, $mimes );
+		return array(
+			'ext'             => $filetype['ext'],
+			'type'            => $filetype['type'],
+			'proper_filename' => $data['proper_filename'],
+		);
+	},
+	10,
+	4
+);
 
-}, 10, 4 );
-
-function cc_mime_types( $mimes ){
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
+function cc_mime_types( $mimes ) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
 
 function fix_svg() {
-  echo '<style type="text/css">
+	echo '<style type="text/css">
         .attachment-266x266, .thumbnail img {
              width: 100% !important;
              height: auto !important;
