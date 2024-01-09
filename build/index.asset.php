b2fb75a0fe5b230ffca1322ce2e8a70703e2b23a
Jason Chafin <Herm71@users.noreply.github.com>
2024-01-09 12:29:52 -0800

    feat: :sparkles: New Features!
    * add `build/` directory to `.gitignore`
    
    * Replace static local urls with staging urls in templates.
    
    * refactor and split styles
    
    * Typo in functions -- wrong function name
    
    * Style Details block, enqueue style.js in functions
    
    * Added excerpt styles
    
    * edit git ignore and remove build dir
    
    * style.css version bump
    
    * final css edits

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '53efcc6769a40b86cf51');
--- build/theme.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f92b3548356c7c3e484a');
